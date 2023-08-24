086d165f10c771eb6edd5ef5a9dee284aa8dd91e
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2023-08-24 21:17:49 +0530

    Add files via upload

---
+++ Assignment1/Fibo_pattern.java
@@ -0,0 +1,43 @@
+package Assignment1;
+import java.util.*;
+public class Fibo_pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		Scanner s= new Scanner(System.in);
+		
+		int n=s.nextInt();
+		
+		int row=1;
+		
+		int nst=1;
+		
+		int a=0;
+		int b=1;
+		
+		 while(row<=n) {
+			 
+			 // star
+			  
+			 int i=1;
+			 
+			 while(i<=nst) {
+				 System.out.print(a+"\t");
+				 
+				 int c=a+b;
+				 a=b;
+				 b=c;
+				 
+				 i++;
+			 }
+			 
+			 // Preparation for the Next Row
+			 
+			  nst=nst+1;
+			  row=row+1;
+			  System.out.println();
+		 }
+	}
+
+}
